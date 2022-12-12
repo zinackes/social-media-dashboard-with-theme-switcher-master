b6092b52028f4f078876dd8ec2dc720d243ad288
zinacke <88772783+zinacke@users.noreply.github.com>
2022-12-12 21:26:18 +0100

    dark-mode

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-const body = document.body.style.backgroundColor;
-const header = document.getElementById("header").style.backgroundColor;
-const content = document.querySelector("#content").style.backgroundColor;
+var header = document.querySelector(".header")
+var title = document.querySelector("#title")
+var background = document.querySelector("body")
+var box = document.querySelectorAll(".content")
+var number = document.querySelectorAll(".content > h1")
+var box2 = document.querySelectorAll(".content_")
+var number2 = document.querySelectorAll(".content_ > h1")
 
-function DarkMode(checkbox){
+function Mode(checkbox){
+
     if(checkbox.checked){
-         "hsl(225, 100%, 98%)";
-         = "hsl(227, 47%, 96%)";
+        header.classList.add("header-light");
+        background.style.backgroundColor = "white";
+        title.classList.add("title-light");
+        for (const content of box){
+            content.classList.add("content-light");
+        }
+        for (const h1 of number){
+            h1.classList.add("number-light");
+        }
+        for (const content_ of box2){
+            content_.classList.add("content_-light");
+        }
+        for (const h1 of number2){
+            h1.classList.add("number-light");
+        }
+    }
+    else{
+        header.classList.remove("header-light");
+        background.removeAttribute("style");
+        title.classList.remove("title-light");
+        for (const content of box){
+            content.classList.remove("content-light");
+        }
+        for (const h1 of number){
+            h1.classList.remove("number-light");
+        }
+        for (const content_ of box2){
+            content_.classList.remove("content_-light");
+        }
+        for (const h1 of number2){
+            h1.classList.remove("number-light");
+        }
     }
 }
