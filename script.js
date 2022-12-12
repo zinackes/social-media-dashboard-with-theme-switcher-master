128f6722c2c4eb9eb3a4ac83b4db74ff688d6388
zinackes <zinacke@gmail.com>
2022-12-12 14:19:48 +0100

    DarkMode first version

---
+++ script.js
@@ -0,0 +1,10 @@
+const body = document.body.style.backgroundColor;
+const header = document.getElementById("header").style.backgroundColor;
+const content = document.querySelector("#content").style.backgroundColor;
+
+function DarkMode(checkbox){
+    if(checkbox.checked){
+         "hsl(225, 100%, 98%)";
+         = "hsl(227, 47%, 96%)";
+    }
+}
