6b35dda0e6db14c3bab9b77777ffbd3b8ee39f3d
zinacke <88772783+zinacke@users.noreply.github.com>
2022-12-13 13:54:32 +0100

    Update

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var title = document.querySelector(".title")
 var background = document.querySelector("body")
 var box = document.querySelectorAll(".content")
 var number = document.querySelectorAll(".content > h1")
-var box2 = document.querySelectorAll(".content_")
-var number2 = document.querySelectorAll(".content_ > h1")
+var boxtwo = document.querySelectorAll(".content_")
+var numbertwo = document.querySelectorAll(".content_ > h1")
 var slider = document.querySelector(".slider")
 
 function Mode(checkbox){
@@ -13,34 +13,26 @@ function Mode(checkbox){
         header.classList.add("header-light");
         background.style.backgroundColor = "white";
         title.classList.add("title-light");
-        for (const content of box){
-            content.classList.add("content-light");
+        for  (let i = 0; i < box.length; i++){
+            box[i].classList.add("content-light");
+            number[i].classList.add("number-light");
         }
-        for (const h1 of number){
-            h1.classList.add("number-light");
-        }
-        for (const content_ of box2){
-            content_.classList.add("content_-light");
-        }
-        for (const h1 of number2){
-            h1.classList.add("number-light");
+        for (let i = 0; i < boxtwo.length; i++){
+            boxtwo[i].classList.add("content_-light");
+            numbertwo[i].classList.add("number-light");
         }
     }
     else{
         header.classList.remove("header-light");
         background.removeAttribute("style");
         title.classList.remove("title-light");
-        for (const content of box){
-            content.classList.remove("content-light");
+        for  (let i = 0; i < box.length; i++){
+            box[i].classList.remove("content-light");
+            number[i].classList.remove("number-light");
         }
-        for (const h1 of number){
-            h1.classList.remove("number-light");
-        }
-        for (const content_ of box2){
-            content_.classList.remove("content_-light");
-        }
-        for (const h1 of number2){
-            h1.classList.remove("number-light");
+        for (let i = 0; i < boxtwo.length; i++){
+            boxtwo[i].classList.remove("content_-light");
+            numbertwo[i].classList.remove("number-light");
         }
     }
 }
